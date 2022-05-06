9d141cc5c6651df002dc236a7fc52ba90d5d0939
harigithub1 <69260548+harigithub1@users.noreply.github.com>
2022-05-06 10:19:42 +0530

    adhoc

--- src/Level1/RepeatedWordWithCount.java
+++ src/Level1/RepeatedWordWithCount.java
@@ -1,31 +1,30 @@
 package Level1;
 
-public class RepeatedWordWithCount
-{
-
-	public static void main(String[] args)
-	{
-		String input="Welcome to Java Session Session one"; 		//Input String	
-		String[] words=input.split(" ");	//Split the word from String
-		int wrc=1;							//Variable for getting Repeated word count
-		
-		for(int i=0;i<words.length;i++)		//Outer loop for Comparison		
-		{
-			for(int j=i+1;j<words.length;j++)	//Inner loop for Comparison
-			{
-				
-			if(words[i].equals(words[j]))	//Checking for both strings are equal
-				{
-					wrc=wrc+1;				//if equal increment the count
-					words[j]="0";			//Replace repeated words by zero
-				}
-			}
-			if(words[i]!="0")
-			System.out.println(words[i]+"--"+wrc);	//Printing the word along with count
-			wrc=1;
-			
-	     }  
-			
-	}
-
+public class RepeatedWordWithCount {
+    public static void main(String[] args) {
+        //input String
+        String input = "Java is widely used in building web application and mobile application";
+        //split the words from String
+        String[] words = input.split(" ");
+        //variable for getting repeated word count
+        int wc = 1;
+        //outer loop for comparison
+        for (int i = 0; i < words.length; i++) {
+            //inner loop for comparison
+            for (int j = i + 1; j < words.length; j++) {
+                //checking if both strings are equal
+                if (words[i].equals(words[j])) {
+                    //if equal then increment the count
+                    wc = wc + 1;
+                    //replace repeated words by zero
+                    words[j] = "0";
+                }
+            }
+            if (words[i] != "0")
+                //printing the word along with count
+                if (wc > 1)
+                    System.out.println("the word " + "'" + words[i] + "'" + " is repeated " + wc + " times");
+            wc = 1;
+        }
+    }
 }
