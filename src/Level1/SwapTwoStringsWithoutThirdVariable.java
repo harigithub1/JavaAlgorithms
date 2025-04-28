71b9afa7c0008ea6037b8a5e0c14c23948db7f73
harigithub1 <m.haribabu26@gmail.com>
2025-04-28 15:38:51 +0530

    improvisations

--- src/Level1/SwapTwoStringsWithoutThirdVariable.java
+++ src/Level1/SwapTwoStringsWithoutThirdVariable.java
@@ -5,7 +5,7 @@ public class SwapTwoStringsWithoutThirdVariable {
         String s1 = "abcd", s2 = "efgh";
         s1 = s1 + s2;
         s2 = s1.substring(0, 4);//or (0,s1.length()-s2.length()) to pass dynamically
-        s1 = s1.substring(3, 7);//or (s2.length) to pass dynamically
+        s1 = s1.substring(4);//or (s2.length) to pass dynamically
         System.out.println("s1 " + s1);
         System.out.println("s2 " + s2);
     }
