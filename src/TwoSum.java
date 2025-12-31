eca4fc8ee2bb4c1154bdfcc1fef30937301b7611
Hari <69260548+harigithub1@users.noreply.github.com>
2025-12-31 23:16:47 +0530

    Update TwoSum.java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class E1TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
