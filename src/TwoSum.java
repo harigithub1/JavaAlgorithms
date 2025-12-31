034dc7833620a442450848a849e03bb9610784f6
Hari <69260548+harigithub1@users.noreply.github.com>
2025-12-31 23:22:29 +0530

    Rename src/TwoSum.java to src/E1TwoSum.java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,23 +1,2 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class E1TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
-            }
-            map.put(nums[i], i);
-        }
-        throw new IllegalArgumentException("No two sum solution");
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 18;
-        int[] result = twoSum(nums, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
-    }
-}
+// Renamed to src/E1TwoSum.java
+// Original implementation moved to src/E1TwoSum.java
