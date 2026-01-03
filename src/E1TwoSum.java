bc3855ac847c180eb779a8ce30059586e1c9b432
harigithub1 <m.haribabu26@gmail.com>
2026-01-03 23:55:51 +0530

    update

--- src/E1TwoSum.java
+++ src/E1TwoSum.java
@@ -17,11 +17,19 @@ public class E1TwoSum {
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
+            //checking if complement is present ant then returning indices
             Integer index = map.get(complement);
             if (index != null) {
                 return new int[] {index, i};
             }
             map.put(nums[i], i);
+
+//            //checking if complement is present ant then returning indices
+//            if (map.containsKey(complement)) {
+//                return new int[]{map.get(complement), i};
+//            }
+//            map.put(numbers[i], i);
+
         }
 
         throw new IllegalArgumentException("No two numbers sum to target: " + target);
