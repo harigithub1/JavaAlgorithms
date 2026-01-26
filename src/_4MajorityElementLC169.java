ac8e254b0cda55a6b550ae3707845587beb4f4fc
Hari <69260548+harigithub1@users.noreply.github.com>
2026-01-27 01:54:08 +0530

    Update _4MajorityElementLC169.java

--- src/_4MajorityElementLC169.java
+++ src/_4MajorityElementLC169.java
@@ -1,33 +1,23 @@
-
 class _4MajorityElementLC169 {
-
     public static int majorityElement(int[] nums) { // 1 usage
-
         int candidate = 0;
         int count = 0;
-
         for (int i = 0; i < nums.length; i++) {
-
             int num = nums[i];
-
             if (count == 0) {
                 candidate = num;
             }
-
             if (num == candidate) {
                 count++;
             } else {
                 count--;
             }
         }
-
         return candidate;
     }
 
     public static void main(String[] args) {
-
         int[] nums = {2, 2, 1, 1, 1, 2, 2, 2};
-
         System.out.println(majorityElement(nums));
     }
 }
