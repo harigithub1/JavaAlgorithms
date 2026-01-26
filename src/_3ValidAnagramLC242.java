e464f6a00044a8b1eaab3fd49392dde936b85029
Hari <69260548+harigithub1@users.noreply.github.com>
2026-01-26 23:40:18 +0530

    Update _3ValidAnagramLC242.java

--- src/_3ValidAnagramLC242.java
+++ src/_3ValidAnagramLC242.java
@@ -31,4 +31,4 @@ public class _3ValidAnagramLC242 {
 //Pattern: Frequency Counting pattern with a fixed-size counting array
 //Time complexity — O(n), Space complexity — O(1)
 
-//here a is used because if other char like b ,c etc are used the index of freq may become negative and negative index is not allowed in array
+//here in first for loop a is used because if other char like b ,c etc are used the index of freq may become negative and negative index is not allowed in array
